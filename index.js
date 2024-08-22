2f9f326d864d36110c47fb6ad30eb838100cfc72
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-23 01:43:59 +0300

    "Deleted files: index.html, index.js, and style.css from Preview-mini calendar directory."

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const dayNameEl = document.getElementById("day-name")
 const dayNumEl = document.getElementById("day-number")
 const yearEl = document.getElementById("year")
 
-// const date = new Date();
-// console.log(date);
+const date = new Date();
+console.log(date);
