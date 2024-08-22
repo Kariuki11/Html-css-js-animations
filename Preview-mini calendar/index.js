485c8cb3acc0221a3a7f203e8ad5b59431dcb308
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-23 01:42:00 +0300

    "Added script tag to index.html and created new index.js file with variable declarations for calendar elements."

---
+++ Preview-mini calendar/index.js
@@ -0,0 +1,7 @@
+const monthNameEl = document.getElementById("month-name")
+const dayNameEl = document.getElementById("day-name")
+const dayNumEl = document.getElementById("day-number")
+const yearEl = document.getElementById("year")
+
+// const date = new Date();
+// console.log(date);
